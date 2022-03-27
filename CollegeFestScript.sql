c00b64f160c74a04ad52b282c1bf5e1a1bb10125
shubhamshukla7794 <shubhamshukla7794@gmail.com>
2022-03-27 18:34:19 +0530

    SQL script added

---
+++ CollegeFestScript.sql
@@ -0,0 +1,8 @@
+INSERT INTO `collegefest`.`roles` (`role_id`,`name`) VALUES (1,'ADMIN');
+INSERT INTO `collegefest`.`roles` (`role_id`,`name`) VALUES (2,'USER');
+
+INSERT INTO `collegefest`.`users` (`user_id`,`password`,`username`) VALUES (1, '$2a$12$W4troexnHC7LIOBSM1nmDuncp3tNmifMyJ58bKS.5zTYyo.g1svFO','admin');
+INSERT INTO `collegefest`.`users` (`user_id`,`password`,`username`) VALUES (2, '$2a$12$9MutAqlt3FwEl2qdGyQPfOhCg18ggEA2rugzQxwmPV7Eb0dgMqbY.','user');
+
+INSERT INTO `collegefest`.`users_roles` (`user_id`,`role_id`) VALUES (1, 1);
+INSERT INTO `collegefest`.`users_roles` (`user_id`,`role_id`) VALUES (2, 2);
